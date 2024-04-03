12e4174901a8825565698a69143dd49cc4ad18bb
Vanja Kujundzic <kujundzicv97@gmail.com>
2024-04-03 16:23:00 +0200

    implement a reset function and a button

--- javascript.js
+++ javascript.js
@@ -1,26 +1,53 @@
 const divBox= document.querySelector("#box");
+const wrapper= document.querySelector(".wrapper")
 
+//Defining the starting grid size
+let gridSize=prompt("how many squares?");
+if (gridSize>100){
+    alert("It has to be less than 100")
+    gridSize=prompt("how many squares?");
+}
+
+
+ //adding a reset btn
+let resetBtn= document.createElement("button");
+ resetBtn.textContent="Reset";
+ wrapper.append(resetBtn);
+ resetBtn.addEventListener("click", () => {
+     gridSize=prompt("How many squares in the grid?");
+     if (gridSize>100){
+        alert("It has to be less than 100")
+        gridSize=prompt("how many squares?");
+    }
+     resetGame();
+     GridLayout();
+     hoverEffect();
+     
+    console.log(gridSize);
+    
+ })
 
 //adding the grid layout inside the box
 function GridLayout(){
-    for(let i=0; i<10;i++) 
+    for(let i=0; i<gridSize;i++) 
         {
-            for (let j = 0; j < 10; j++) {
+            for (let j = 0; j < gridSize; j++) {
 
                 let square= document.createElement("div");
                 square.classList.add("square")
                 // square.style.setProperty("flex","1 0 0")
                 divBox.append(square);
-                square.style.width= `calc(${100/10-0.398}%)` //calculates the width of the square to make them fit inside the box
-                
-                
+                square.style.width= `calc(${100/gridSize-0.398}%)` //calculates the width of the square to make them fit inside the box  
             }
-
-           
-               
-            
+  
         }   
  }
+ GridLayout();
+ hoverEffect();
+ 
+ 
+
+
 //creating the hover effect for the grid box that increases the opacity with each mouse hover
 let currentOpacity=0.1;
 function hoverEffect(){
@@ -42,6 +69,25 @@ squares.forEach(square => {
 })})
 }
 
-GridLayout();
-hoverEffect();
+function resetGame(){
+    
+    currentOpacity= 0.0;
+    let squares= document.querySelectorAll(".square")
+    squares.forEach(square => {
+    square.dataset.opacity=0.1;
+    square.style.backgroundColor= `rgba(0,0,0, ${currentOpacity})`;
+    })
+
+    removeChildNodes(divBox);
+
+}
+function removeChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+    
+}
+
+
+
 
