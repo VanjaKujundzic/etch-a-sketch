dd70ab2e296c5e52d47d5ca492f5cbe9693d4fb1
Vanja Kujundzic <kujundzicv97@gmail.com>
2024-04-03 14:40:07 +0200

    add a function that colors the squares

--- javascript.js
+++ javascript.js
@@ -3,26 +3,45 @@ const divBox= document.querySelector("#box");
 
 //adding the grid layout inside the box
 function GridLayout(){
-    for(let i=0; i<6;i++) 
+    for(let i=0; i<10;i++) 
         {
-            for (let j = 0; j < 6; j++) {
+            for (let j = 0; j < 10; j++) {
 
                 let square= document.createElement("div");
                 square.classList.add("square")
                 // square.style.setProperty("flex","1 0 0")
                 divBox.append(square);
-                square.style.width= `calc(${100/6-0.8}%)`
+                square.style.width= `calc(${100/10-0.398}%)` //calculates the width of the square to make them fit inside the box
+                
                 
             }
 
            
-                // for(let j=0;j<=i;j++){
-                //     square=document.createElement("div");
-                //     square.classList.add("squareColumn")
-                //     divBox.append(square);
-                // }
+               
             
-        }
+        }   
  }
 
+let currentOpacity=0.1;
+function hoverEffect(){
+let squares=document.querySelectorAll(".square")
+squares.forEach(square => {
+    square.dataset.opacity= 0.2; //adds a starting value of each square
+
+    square.addEventListener("mouseover", () => {
+    currentOpacity= parseFloat(square.dataset.opacity)+0.3; //each square when the mouse moves adds 0.3 opacity to the current value
+
+    if(currentOpacity>1){  
+        currentOpacity=1;
+    }
+    square.dataset.opacity=currentOpacity; //updates the value of current so it know for next time mouse passes by a square what was the last value
+    square.style.backgroundColor= `rgba(0,0,0, ${currentOpacity})`;
+    
+    
+
+})})
+}
+
 GridLayout();
+hoverEffect();
+
